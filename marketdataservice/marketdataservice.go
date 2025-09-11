fa232c7274eb00595af3fb99ecd28a7a7761ab79
Dmitry Lazarev <diman.lazarev@gmail.com>
2025-09-11 10:25:46 +0300

    GetLastPrices() is done!

--- marketdataservice/marketdataservice.go
+++ marketdataservice/marketdataservice.go
@@ -4,26 +4,30 @@ import (
 	"encoding/json"
 	"ldv/tinvest"
 	"log"
-	"strings"
 )
 
 type LastPrice struct {
 	Price         tinvest.Amount
 	Time          string
-	InstrumentId  string
+	InstrumentUid string
 	LastPriceType string
 }
 type Prices struct {
 	LastPrices []LastPrice
 }
 
-func GetLastPrices(token string, figies []string) Prices {
+func GetLastPrices(token string, instrumentId []string) Prices {
 	var url = `https://invest-public-api.tbank.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetLastPrices`
 	var lps Prices
 
-	payload := "{ [" + strings.Join(figies, ", ") + "],}"
+	i, err := json.Marshal(instrumentId)
+	if err != nil {
+		log.Fatalf("Error in GetLastPrices(): %v", err)
+	}
+
+	payload := `{ "instrumentId":` + string(i) + `}`
 	data := tinvest.GetAPIRequest(url, token, payload)
-	err := json.Unmarshal([]byte(data), &lps)
+	err = json.Unmarshal([]byte(data), &lps)
 	if err != nil {
 		log.Fatal(err)
 	}
